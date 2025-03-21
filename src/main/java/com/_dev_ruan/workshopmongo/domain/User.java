e49cefff912ba147fe2b9ed8a0319c21dfaa9dd9
ruan-dev77 <ruansaraiva555@gmail.com>
2025-03-20 21:32:33 -0300

    Conectando ao mongoDB com repository e servide

--- src/main/java/com/_dev_ruan/workshopmongo/domain/User.java
+++ src/main/java/com/_dev_ruan/workshopmongo/domain/User.java
@@ -3,13 +3,18 @@ package com._dev_ruan.workshopmongo.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user")
 public class User implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private Integer id;
+	@Id
+	private String id;
 	private String name;
 	private String email;
 	
@@ -17,17 +22,17 @@ public class User implements Serializable {
 		
 	}
 
-	public User(Integer id, String name, String email) {
+	public User(String id, String name, String email) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
 
-	public Integer getId() {
+	public String getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(String id) {
 		this.id = id;
 	}
 
--- src/main/java/com/_dev_ruan/workshopmongo/resources/UserReosurce.java
+++ src/main/java/com/_dev_ruan/workshopmongo/resources/UserReosurce.java
@@ -4,25 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com._dev_ruan.workshopmongo.domain.User;
+import com._dev_ruan.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/Users")
 public class UserReosurce {
+	@Autowired
+	UserService serv;
 	
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		User maria = new User(1,"mAria silva", "maria@gmail.com");
-		User alex = new User(2,"Alex mato", "alex@gamil.com");
-		
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,alex));
+	public ResponseEntity<List<User>> findAll() {	
+		List<User> list = serv.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
